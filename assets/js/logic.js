ce915b386f6de70e362523e601135cd02c60f865
mhorton20916 <schizoelephant@gmail.com>
2018-06-01 15:56:39 -0500

    Added recent data to existing map

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -9,171 +9,75 @@ var map = L.map('map',{
 })
 
 
+var allData = [];
 
-var borders = [];
-
-var _1960Data = {type:'FeatureCollection1',
-                 features: []
-                };
-var _1970Data = {type:'FeatureCollection2',
-                features: []
-                };
-var _1980Data = {type:'FeatureCollection3',
-                features: []
-                };
-var _1990Data = {type:'FeatureCollection4',
-                features: []
-                };
-var _1960Layer;
-var _1970Layer;
-var _1980Layer;
-var _1990Layer;
-
-var allLayers;
-
-
+var allLayers = {};
 
 var feature = [];
 
 d3.json('/data/custom.geo.json',function(error,data) {
     if(error){console.warn(error)};
-    // console.log(data)
-    // let features = data.features;
-    // let testItem = features[0]['properties'];
-    // console.log(testItem)
-    // testItem['test']='TESTING';
-    // console.log(testItem)
-    // console.log(features.map(x=>x.properties.name))
-    // features.forEach(x=>borders.push(x.geometry));
-    // let borderLayer = L.geoJSON(borders);
-    // borderLayer.addTo(map)
+
     d3.json('/data/immigration_by_country.JSON',function(error,cdata) {
         if(error){console.warn(error)};
         // console.log(cdata);
         // console.log(cdata['1960'])
+        for(year in cdata) {
+            //Initializes the list allData, and creates a feature collection for each year found in cdata
+            allData[year] = {type:'FeatureCollection',features:[]}
+        }
+        // console.log(allData)
         for(elem in data.features) {
             feature = data.features[elem];
-            // console.log(feature)
-            if(cdata['1960'][feature.properties.name]) {
-                let immigrants = parseInt(cdata['1960'][feature.properties.name].replace(/,/g, ''));
-                //This JSON.parse(JSON.stringify()) bullshit allowed me to bypass the horseshit shallow copy
-                //thing that javascript kept insisting I meant.  No I don't wanna update feature, that's why
-                //I saved it into temporary feature.  wtf get your shit together js
-                let tempfeat = JSON.parse(JSON.stringify(feature));
-                tempfeat['properties']['immigrants'] = immigrants;
-                _1960Data.features.push(tempfeat)
-                // data.features[elem]['properties']['immigrants']= immigrants;
-
-            }
-            if(cdata['1970'][feature.properties.name]) {
-                let immigrants = parseInt(cdata['1970'][feature.properties.name].replace(/,/g, ''));
-                let tempfeat = JSON.parse(JSON.stringify(feature));
-                tempfeat['properties']['immigrants'] = immigrants;
-                _1970Data.features.push(tempfeat)
-                // data.features[elem]['properties']['immigrants']= immigrants;
-
-            }
-            if(cdata['1980'][feature.properties.name]) {
-                let immigrants = parseInt(cdata['1980'][feature.properties.name].replace(/,/g, ''));
-                let tempfeat = JSON.parse(JSON.stringify(feature));
-                tempfeat['properties']['immigrants'] = immigrants;
-                _1980Data.features.push(tempfeat)
-                // data.features[elem]['properties']['immigrants']= immigrants;
-
-            }
-            if(cdata['1990'][feature.properties.name]) {
-                let immigrants = parseInt(cdata['1990'][feature.properties.name].replace(/,/g, ''));
-                let tempfeat = JSON.parse(JSON.stringify(feature));
-                tempfeat['properties']['immigrants'] = immigrants;
-                _1990Data.features.push(tempfeat)
-                // data.features[elem]['properties']['immigrants']= immigrants;
- 
+            // console.log(feature.properties.name)
+            for(year in cdata) {
+                if(cdata[year][feature.properties.name]) {
+                    let immigrants = parseInt(cdata[year][feature.properties.name].replace(/,/g, ''));
+                    //This JSON.parse(JSON.stringify()) bullshit allowed me to bypass the horseshit shallow copy
+                    //thing that javascript kept insisting I meant.  No I don't wanna update feature, that's why
+                    //I saved it into temporary feature.  wtf get your shit together js
+                    let tempfeat = JSON.parse(JSON.stringify(feature));
+                    tempfeat['properties']['immigrants'] = immigrants;
+                    //If statement checks to see if value is valid or NaN
+                    if(immigrants) {
+                        allData[year].features.push(tempfeat)
+                    }
+                }
             }
         }
-        // console.log(_1960Data)
-        // console.log(_1970Data)
-        // console.log(_1980Data)
-        // console.log(_1990Data)
+        // console.log(allData)
 
-        _1960Layer = L.choropleth(_1960Data,{
-            valueProperty: 'immigrants',
-            scale: ["#ffffb2", "#b10026"],
-            steps: 10,
-            mode: 'q',
-            style: {
-                // Border color
-                color: "#fff",
-                weight: 1,
-                fillOpacity: 0.8
-            },
-
-            onEachFeature: function(feature, layer) {
-            layer.bindPopup(feature.properties.name + "<br>Number of 1960s Immigrants:<br>" + feature.properties.immigrants);
-            }
-        });
-        _1970Layer = L.choropleth(_1970Data,{
-            valueProperty: 'immigrants',
-            scale: ["#ffffb2", "#b10026"],
-            steps: 10,
-            mode: 'q',
-            style: {
-                // Border color
-                color: "#fff",
-                weight: 1,
-                fillOpacity: 0.8
-            },
-
-            onEachFeature: function(feature, layer) {
-            layer.bindPopup(feature.properties.name + "<br>Number of 1970s Immigrants:<br>" + feature.properties.immigrants);
-            }
-        });
-        _1980Layer = L.choropleth(_1980Data,{
-            valueProperty: 'immigrants',
-            scale: ["#ffffb2", "#b10026"],
-            steps: 10,
-            mode: 'q',
-            style: {
-                // Border color
-                color: "#fff",
-                weight: 1,
-                fillOpacity: 0.8
-            },
-
-            onEachFeature: function(feature, layer) {
-            layer.bindPopup(feature.properties.name + "<br>Number of 1980s Immigrants:<br>" + feature.properties.immigrants);
-            }
-        });
-        _1990Layer = L.choropleth(_1990Data,{
-            valueProperty: 'immigrants',
-            scale: ["#ffffb2", "#b10026"],
-            steps: 10,
-            mode: 'q',
-            style: {
-                // Border color
-                color: "#fff",
-                weight: 1,
-                fillOpacity: 0.8
-            },
-
-            onEachFeature: function(feature, layer) {
-            layer.bindPopup(feature.properties.name + "<br>Number of 1990s Immigrants:<br>" + feature.properties.immigrants);
-            }
-        });
-
-        allLayers = {'1960':_1960Layer,
-                    '1970':_1970Layer,
-                    '1980':_1980Layer,
-                    '1990':_1990Layer};
+        for(year in allData) {
+            // console.log(year)
+            allLayers[year] = L.choropleth(allData[year],{
+                valueProperty: 'immigrants',
+                scale: ["#ffffb2", "#b10026"],
+                steps: 10,
+                mode: 'q',
+                style: {
+                    // Border color
+                    color: "#fff",
+                    weight: 1,
+                    fillOpacity: 0.8
+                },
+    
+                onEachFeature: function(feature, layer) {
+                layer.bindPopup(feature.properties.name + `<br>Number of Immigrants in ${year}:<br>` + feature.properties.immigrants);
+                }
+            });
+        }
+        // console.log(allLayers)
+        
         L.control.layers(allLayers).addTo(map)
         var legend = L.control({ position: "bottomright" });
         legend.onAdd = function() {
             var div = L.DomUtil.create("div", "info legend");
-            var limits = _1960Layer.options.limits;
-            var colors = _1960Layer.options.colors;
+            var limits = allLayers['1960'].options.limits;
+            var colors = allLayers['1960'].options.colors;
             var labels = [];
             // console.log(limits)
             // Add min & max
-            var legendInfo = "<h1>Immigration Volume</h1>" +
+            var legendInfo = "<h1 style='text-align:center;'>Immigration Volume</h1>" +
             "<div class=\"labels\">" +
                 "<div class=\"min\">" + limits[0] + "</div>" +
                 "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
@@ -192,6 +96,7 @@ d3.json('/data/custom.geo.json',function(error,data) {
     })
 })
 
+//Following code updates legend after user changes choropleth layer
 map.on('baselayerchange',function(e) {
     // console.log('change')
     // console.log(e)
@@ -200,7 +105,7 @@ map.on('baselayerchange',function(e) {
     let colors = allLayers[e.name].options.colors;
     let labels = [];
 
-    let legendInfo = "<h1>Immigration Volume</h1>" +
+    let legendInfo = "<h1 style='text-align:center;'>Immigration Volume</h1>" +
         "<div class=\"labels\">" +
         "<div class=\"min\">" + limits[0] + "</div>" +
         "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
